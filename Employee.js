31a90ab0f6f7f4a06c6cf47bc2465279ba01c9a6
Chris Kuiper <chris.kuiper@gmail.com>
2016-10-17 13:20:18 -0500

    add constructor, promote and parseFromFilePath code.

--- Employee.js
+++ Employee.js
@@ -1,16 +1,28 @@
 'use strict'
 
+// import { readFile, writeFile } from 'fs'
+
 const fs = require('fs')
 
 class Employee {
-  constructor (/* ??? */) {
-    // TODO
+  constructor (name, title, salary) {
+    this.name = name
+    this.title = title
+    this.salary = salary
   }
 
-  // TODO ???
-}
+  promote (title, salary) {
+    this.title = title
+    this.salary = salary
+  }
 
-// TODO ???
+  static parseFromFilePath (path) {
+    let source = fs.readFileSync(path).toString()
+    let obj = JSON.parse(source)
+    console.log(source)
+    return new Employee(obj.name, obj.title, obj.salary)
+  }
+}
 
 module.exports = {
   Employee
